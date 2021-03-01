49413f8cc0622cec5eea2c85d90b853cf70e2fb0
Yuning Shi <shiyuning@gmail.com>
2021-03-01 13:21:25 -0500

    :bug: Fix #5 - increase stability using backward-in-time for transport
    Previous scheme used forward-in-time to solve transport. It caused instability in some situations. Thus switch to backward-in-time scheme to increase stability

--- src/transpt.c
+++ src/transpt.c
@@ -1,84 +1,62 @@
 #include "biort.h"
 
-// Calculates mass in the snow + soil zone, and the transport of mass due to recharge
+// Calculates mass from transport
+// To increase stability, transport is calculated using backward-in-time scheme, in which
+//   d(CV) / dt = C_in * q_in - C * q_out + f
+// i.e.,
+//   C * V - C_0 * V_0 = C_in * Q_in - C * Q_out + F.
+// Thus,
+//   C = (C_in * Qin + F + C0 * V0) / (V + Q_out).
 void Transpt(int step, int nsub, rttbl_struct *rttbl, subcatch_struct subcatch[])
 {
     int             ksub;
     int             kspc;
-    double          perc;
-    double          perc1;
-    double          conc_rechg;
+    double          conc_temp;
 
     for (ksub = 0; ksub < nsub; ksub++)
     {
-        if (step > 0)
-        {
-            // Percolation rate is not an output variable in HBV-Light. Calculate percolation rate first using mass
-            //balance
-            perc = subcatch[ksub].ws[step][LZ] - subcatch[ksub].ws[step - 1][LZ] + subcatch[ksub].q[step][Q2];
-
-            perc = MIN(perc, subcatch[ksub].perc);
-            perc = MAX(perc, 0.0);
-
-            if (subcatch[ksub].ws[step][UZ] - 5.0 > 0.0)
-            {
-                perc1 = subcatch[ksub].perc;
-            }
-            else
-            {
-                perc1 = subcatch[ksub].q[step][Q1] / subcatch[ksub].k1 - subcatch[ksub].q[step][Q1];
-            }
-        }
-
         // Transport for primary species
         for (kspc = 0; kspc < rttbl->num_spc; kspc++)
         {
-            if (step > 0)
-            {
-                // Precipitation
-                subcatch[ksub].chms[SNSM].tot_mol[kspc] +=
-                    subcatch[ksub].prcp_conc[kspc] * subcatch[ksub].q[step][PRECIP];
+            // Precipitation
+            subcatch[ksub].chms[SNSM].tot_mol[kspc] +=
+                subcatch[ksub].prcp_conc[kspc] * subcatch[ksub].q[step][PRECIP];
 
-                // Recharge
-                // When there is water in snow/soil moisture storage, use snow/soil moisture concentration for recharge
-                // When there is no water in snow/soil moisture storage, use precipitation concentration for recharge
-                conc_rechg = (subcatch[ksub].ws[step - 1][SNSM] > 0.0) ?
-                    subcatch[ksub].chms[SNSM].tot_conc[kspc] : subcatch[ksub].prcp_conc[kspc];
+            // Temperary concentration in snow/soil
+            conc_temp = subcatch[ksub].chms[SNSM].tot_mol[kspc] /
+                (subcatch[ksub].ws[step][SNSM] + subcatch[ksub].q[step][RECHG]);
 
-                subcatch[ksub].chms[SNSM].tot_mol[kspc] -= conc_rechg * subcatch[ksub].q[step][RECHG];
-                subcatch[ksub].chms[UZ].tot_mol[kspc] += conc_rechg * subcatch[ksub].q[step][RECHG];
+            // Recharge
+            subcatch[ksub].chms[SNSM].tot_mol[kspc] -= conc_temp * subcatch[ksub].q[step][RECHG];
+            subcatch[ksub].chms[UZ].tot_mol[kspc] += conc_temp * subcatch[ksub].q[step][RECHG];
 
-                // Q0
-                // Note that Q0 should always use snow/soil concentration (or precipitation concentration if there is no
-                // snow/soil water)
-                subcatch[ksub].chms[UZ].tot_mol[kspc] -= conc_rechg * subcatch[ksub].q[step][Q0];
+            // Q0
+            subcatch[ksub].chms[UZ].tot_mol[kspc] -= conc_temp * subcatch[ksub].q[step][Q0];
+            subcatch[ksub].chms[STREAM].tot_mol[kspc] = conc_temp * subcatch[ksub].q[step][Q0];
 
-                // Q1
-                subcatch[ksub].chms[UZ].tot_mol[kspc] -=
-                    subcatch[ksub].chms[UZ].tot_conc[kspc] * subcatch[ksub].q[step][Q1];
+            // Temperary concentration in UZ
+            conc_temp = subcatch[ksub].chms[UZ].tot_mol[kspc] /
+                (subcatch[ksub].ws[step][UZ] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][PERC]);
 
-                // Percolation
-                subcatch[ksub].chms[UZ].tot_mol[kspc] -= subcatch[ksub].chms[UZ].tot_conc[kspc] * perc;
-                subcatch[ksub].chms[LZ].tot_mol[kspc] += subcatch[ksub].chms[UZ].tot_conc[kspc] * perc;
+            // Q1
+            subcatch[ksub].chms[UZ].tot_mol[kspc] -= conc_temp * subcatch[ksub].q[step][Q1];
+            subcatch[ksub].chms[STREAM].tot_mol[kspc] += conc_temp * subcatch[ksub].q[step][Q1];
 
-                // Q2
-                subcatch[ksub].chms[LZ].tot_mol[kspc] -=
-                    subcatch[ksub].chms[LZ].tot_conc[kspc] * subcatch[ksub].q[step][Q2];
-            }
+            // Percolation
+            subcatch[ksub].chms[UZ].tot_mol[kspc] -= conc_temp * subcatch[ksub].q[step][PERC];
+            subcatch[ksub].chms[LZ].tot_mol[kspc] += conc_temp * subcatch[ksub].q[step][PERC];
 
+            // Temperary concentration in LZ
+            conc_temp = subcatch[ksub].chms[LZ].tot_mol[kspc] /
+                (subcatch[ksub].ws[step][LZ] + subcatch[ksub].q[step][Q2]);
 
-            // ROUTING
-            subcatch[ksub].chms[STREAM].tot_mol[kspc] = conc_rechg * subcatch[ksub].q[step][Q0];
-
-            subcatch[ksub].chms[STREAM].tot_mol[kspc] +=
-                subcatch[ksub].chms[UZ].tot_conc[kspc] * subcatch[ksub].q[step][Q1];
-
-            subcatch[ksub].chms[STREAM].tot_mol[kspc] +=
-                subcatch[ksub].chms[LZ].tot_conc[kspc] * subcatch[ksub].q[step][Q2];
+            // Q2
+            subcatch[ksub].chms[LZ].tot_mol[kspc] -= conc_temp * subcatch[ksub].q[step][Q2];
+            subcatch[ksub].chms[STREAM].tot_mol[kspc] += conc_temp * subcatch[ksub].q[step][Q2];
 
             // UPDATE CONCENTRATIONS
-            subcatch[ksub].chms[SNSM].tot_conc[kspc] = (subcatch[ksub].ws[step][SNSM] > 0.0) ?
-                subcatch[ksub].chms[SNSM].tot_mol[kspc] / subcatch[ksub].ws[step][SNSM] : ZERO_CONC;
+            subcatch[ksub].chms[SNSM].tot_conc[kspc] =
+                subcatch[ksub].chms[SNSM].tot_mol[kspc] / subcatch[ksub].ws[step][SNSM];
             subcatch[ksub].chms[UZ].tot_conc[kspc] =
                 subcatch[ksub].chms[UZ].tot_mol[kspc] / subcatch[ksub].ws[step][UZ];
             subcatch[ksub].chms[LZ].tot_conc[kspc] =
