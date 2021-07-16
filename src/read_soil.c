9c9f64d30a3436aa511ed02248bd95869dd02e92
KayalvizhiSadayappan <70716152+KayalvizhiSadayappan@users.noreply.github.com>
2021-07-16 13:41:30 -0400

    Change in Naming Convention
    Immobile term is changed to passive and mobile term to dynamic.

--- src/read_soil.c
+++ src/read_soil.c
@@ -12,7 +12,7 @@ void ReadSoil(const char dir[], int nsub, subcatch_struct sub[])
     fp = fopen(fn, "r");
 
     biort_printf(VL_NORMAL, "SOIL PARAMTERS\n");
-    
+
     NextLine(fp, cmdstr, &lno);   // 2021-05-14
     ReadParam(cmdstr, "POROSITY_SURFACE", 'd', fn, lno, &sub[0].porosity_surface);
     biort_printf(VL_NORMAL, "  Surface porosity is %.2f m3 m-3.\n", sub[0].porosity_surface);
@@ -24,33 +24,33 @@ void ReadSoil(const char dir[], int nsub, subcatch_struct sub[])
     NextLine(fp, cmdstr, &lno);
     ReadParam(cmdstr, "POROSITY_LZ", 'd', fn, lno, &sub[0].porosity_lz);
     biort_printf(VL_NORMAL, "  Lower zone porosity is %.2f m3 m-3.\n", sub[0].porosity_lz);
-    
+
     NextLine(fp, cmdstr, &lno);  // 2021-05-14
-    ReadParam(cmdstr, "WS_IMMOBILE_SURFACE", 'd', fn, lno, &sub[0].res_surface);
+    ReadParam(cmdstr, "WS_PASSIVE_SURFACE", 'd', fn, lno, &sub[0].res_surface);
     sub[0].res_surface = MAX(sub[0].res_surface, STORAGE_MIN);
-    biort_printf(VL_NORMAL, "  Surface residual moisture is %.2f mm.\n", sub[0].res_surface);
+    biort_printf(VL_NORMAL, "  Surface passive water storage is %.2f mm.\n", sub[0].res_surface);
 
     NextLine(fp, cmdstr, &lno);
-    ReadParam(cmdstr, "WS_IMMOBILE_UZ", 'd', fn, lno, &sub[0].res_uz);
+    ReadParam(cmdstr, "WS_PASSIVE_UZ", 'd', fn, lno, &sub[0].res_uz);
     sub[0].res_uz = MAX(sub[0].res_uz, STORAGE_MIN);
-    biort_printf(VL_NORMAL, "  Upper zone residual moisture is %.2f mm.\n", sub[0].res_uz);
+    biort_printf(VL_NORMAL, "  Upper zone passive water storage is %.2f mm.\n", sub[0].res_uz);
 
     NextLine(fp, cmdstr, &lno);
-    ReadParam(cmdstr, "WS_IMMOBILE_LZ", 'd', fn, lno, &sub[0].res_lz);
+    ReadParam(cmdstr, "WS_PASSIVE_LZ", 'd', fn, lno, &sub[0].res_lz);
     sub[0].res_lz = MAX(sub[0].res_lz, STORAGE_MIN);
-    biort_printf(VL_NORMAL, "  Lower zone residual moisture is %.2f mm.\n", sub[0].res_lz);
-    
+    biort_printf(VL_NORMAL, "  Lower zone passive water storage is %.2f mm.\n", sub[0].res_lz);
+
     NextLine(fp, cmdstr, &lno);  // 2021-05-14
     ReadParam(cmdstr, "WS_MAX_SURFACE", 'd', fn, lno, &sub[0].d_surface);
-    biort_printf(VL_NORMAL, "  Surface depth is %.2f mm.\n", sub[0].d_surface);
+    biort_printf(VL_NORMAL, "  Surface maximum total water storage is %.2f mm.\n", sub[0].d_surface);
 
     NextLine(fp, cmdstr, &lno);
     ReadParam(cmdstr, "WS_MAX_UZ", 'd', fn, lno, &sub[0].d_uz);
-    biort_printf(VL_NORMAL, "  Upper zone depth is %.2f mm.\n", sub[0].d_uz);
+    biort_printf(VL_NORMAL, "  Upper zone maximum total water storage is %.2f mm.\n", sub[0].d_uz);
 
     NextLine(fp, cmdstr, &lno);
     ReadParam(cmdstr, "WS_MAX_LZ", 'd', fn, lno, &sub[0].d_lz);
-    biort_printf(VL_NORMAL, "  Lower zone depth is %.2f mm.\n", sub[0].d_lz);
+    biort_printf(VL_NORMAL, "  Lower zone maximum total water storage is %.2f mm.\n", sub[0].d_lz);
 
     fclose(fp);
 }
