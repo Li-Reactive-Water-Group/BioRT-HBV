7bd474d71960db5cf8b17a1d91e07652726de336
KayalvizhiSadayappan <70716152+KayalvizhiSadayappan@users.noreply.github.com>
2021-11-16 09:42:25 -0500

    Update transpt.c
    Bug correction: Earlier primary concentration of primary species were wrongly used to update total concentration of primary species in stream.

--- src/transpt.c
+++ src/transpt.c
@@ -99,9 +99,12 @@ void Transpt(int step, int nsub, rttbl_struct *rttbl, const ctrl_struct *ctrl, s
             //    (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2] > 0.0) ?
             //    subcatch[ksub].chms[STREAM].tot_mol[kspc] /
             //    (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2]) : ZERO_CONC;
-            
-           
-            
+
+            subcatch[ksub].chms[STREAM].tot_conc[kspc] =
+                (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2] > 0.0) ?
+                (subcatch[ksub].chms[SURFACE].tot_conc[kspc] * subcatch[ksub].q[step][Q0] +  subcatch[ksub].chms[UZ].tot_conc[kspc] * subcatch[ksub].q[step][Q1] + subcatch[ksub].chms[LZ].tot_conc[kspc] * subcatch[ksub].q[step][Q2]) / (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2]) : ZERO_CONC;
+
+
         }
     }
 }
@@ -122,11 +125,7 @@ void UpdatePrimConc(int step, int nsub, const rttbl_struct *rttbl, const ctrl_st
                 subcatch[ksub].chms[UZ].prim_conc[kspc] = subcatch[ksub].chms[UZ].tot_conc[kspc];
                 subcatch[ksub].chms[LZ].prim_conc[kspc] = subcatch[ksub].chms[LZ].tot_conc[kspc];
             }
-            subcatch[ksub].chms[STREAM].tot_conc[kspc] =
-                (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2] > 0.0) ?
-                (subcatch[ksub].chms[SURFACE].prim_conc[kspc] * subcatch[ksub].q[step][Q0] +  subcatch[ksub].chms[UZ].prim_conc[kspc] * subcatch[ksub].q[step][Q1] + subcatch[ksub].chms[LZ].prim_conc[kspc] * subcatch[ksub].q[step][Q2]) / (subcatch[ksub].q[step][Q0] + subcatch[ksub].q[step][Q1] + subcatch[ksub].q[step][Q2]) : ZERO_CONC;
 
-            
             subcatch[ksub].chms[STREAM].prim_conc[kspc] = subcatch[ksub].chms[STREAM].tot_conc[kspc];
         }
     }
